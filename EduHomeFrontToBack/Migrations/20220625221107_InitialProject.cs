9067e0354c0a5eb252b18c94b32277f25dc0fe8c
CavadovT <turalkhj@code.edu.az>
2022-06-26 02:12:43 +0400

    feat:added migration

---
+++ EduHomeFrontToBack/Migrations/20220625221107_InitialProject.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EduHomeFrontToBack.Migrations
+{
+    public partial class InitialProject : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Biografiys",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ImgUrlHead = table.Column<string>(nullable: true),
+                    ImgUrlFoot = table.Column<string>(nullable: true),
+                    Copyright = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<decimal>(nullable: false),
+                    email = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Biografiys", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "InformationBoards",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    Subtitle = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InformationBoards", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NoticeBoards",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    createdTime = table.Column<DateTime>(nullable: false),
+                    Content = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NoticeBoards", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SliderContents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SliderContents", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sliders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ImgUrl = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sliders", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Biografiys");
+
+            migrationBuilder.DropTable(
+                name: "InformationBoards");
+
+            migrationBuilder.DropTable(
+                name: "NoticeBoards");
+
+            migrationBuilder.DropTable(
+                name: "SliderContents");
+
+            migrationBuilder.DropTable(
+                name: "Sliders");
+        }
+    }
+}
